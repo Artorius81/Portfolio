5b864f7a59ad406ef27d138879f958f6da85551a
Artorius <Artorius.81@yandex.ru>
2023-05-30 20:01:22 +1000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,45 @@
-document.querySelectorAll('.slider').forEach((n, i) => {
-	window[`slider${i+1}`] = new Swiper(n, {
-		freeMode: true,
-		centeredSlides: true,
-		direction: 'vertical',
-		mousewheel: true,
-		slidesPerView: 1.75,
-		slidesOffsetBefore: -125
+// 3D Scroll
+
+let zSpacing = -1000,
+		lastPos = zSpacing / 5,
+		$frames = document.getElementsByClassName('frame'),
+		frames = Array.from($frames),
+		zVals = []
+
+window.onscroll = function() {
+
+	let top = document.documentElement.scrollTop,
+			delta = lastPos - top
+
+	lastPos = top
+
+	frames.forEach(function(n, i) {
+		zVals.push((i * zSpacing) + zSpacing)
+		zVals[i] += delta * -5.5
+		let frame = frames[i],
+				transform = `translateZ(${zVals[i]}px)`,
+				opacity = zVals[i] < Math.abs(zSpacing) / 1.8 ? 1 : 0
+		frame.setAttribute('style', `transform: ${transform}; opacity: ${opacity}`)
 	})
+
+}
+
+window.scrollTo(0, 1)
+
+// Audio
+
+let soundButton = document.querySelector('.soundbutton'),
+		audio = document.querySelector('.audio')
+
+soundButton.addEventListener('click', e => {
+	soundButton.classList.toggle('paused')
+	audio.paused ? audio.play() : audio.pause()
 })
-bindSwipers(slider1, slider2, slider3, slider4)
+
+window.onfocus = function() {
+	soundButton.classList.contains('paused') ? audio.pause() : audio.play()
+}
+
+window.onblur = function() {
+	audio.pause()
+}
